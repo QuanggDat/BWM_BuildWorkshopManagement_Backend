84e210d8166740dabad901709879e51a3a5bf015
Phan Thanh Duc <deadworld128@gmail.com>
2023-10-03 04:38:04 +0700

    Minor Update

---
+++ Data/Migrations/20231002213239_MinorUpdate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    public partial class MinorUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDeleted",
+                table: "ItemCategory",
+                newName: "isDeleted");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "isDeleted",
+                table: "ItemCategory",
+                newName: "IsDeleted");
+        }
+    }
+}
