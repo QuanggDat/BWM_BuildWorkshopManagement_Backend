0b9dc45e32cf99bd52c0fc431262b9b43b346002
Phan Thanh Duc <deadworld128@gmail.com>
2023-11-12 00:02:22 +0700

    fix role null

--- Sevices/Core/UserService/UserService.cs
+++ Sevices/Core/UserService/UserService.cs
@@ -721,7 +721,7 @@ namespace Sevices.Core.UserService
 
             try
             {
-                var listUser = _dbContext.User
+                var listUser = _dbContext.User.Include(r=>r.Role)
                    .OrderBy(x => x.fullName).ToList();
 
                 if (!string.IsNullOrEmpty(search))
